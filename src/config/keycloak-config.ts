717a51d9a5f192f00d5531c60d4ea28d45942597
Yuanming Chen <yuanming@hotmail.com>
2021-03-22 23:27:01 -0600

    No need to store secrets here

--- src/config/keycloak-config.ts
+++ src/config/keycloak-config.ts
@@ -3,12 +3,9 @@ import bodyStore from "../keycloak-koa-connect/stores/body-store";
 import queryStore from "../keycloak-koa-connect/stores/query-store";
 
 export const keycloak = new KeycloakConnect({ store: [queryStore, bodyStore,] }, {
-    "realm": "Demo-Realm",
-    "serverUrl": "http://localhost:8080/auth",
+    realm: "Demo-Realm",
+    serverUrl: "http://localhost:8080/auth",
     bearerOnly: true,
-    "clientId": "nodejs-microservice",
-    "credentials": {
-        "secret": "1c6fe190-835f-40d7-ac8d-abc232623353"
-    }
+    clientId: "nodejs-microservice"
 });
 
